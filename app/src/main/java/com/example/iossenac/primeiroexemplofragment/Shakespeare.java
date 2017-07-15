0111c72e540c02e15c5bc4f9cca51b278ba34c2e
AlanCourt <alancourt.rs@gmail.com>
2017-07-15 13:19:44 -0300

    ExemploFragment

---
+++ app/src/main/java/com/example/iossenac/primeiroexemplofragment/Shakespeare.java
@@ -0,0 +1,20 @@
+package com.example.iossenac.primeiroexemplofragment;
+
+/**
+ * Created by iossenac on 15/07/17.
+ */
+
+public class Shakespeare {
+
+    public static String[] TITLES = new String[]{
+
+            "Romeu e Julieta",
+            "Sonhos de uma noite de verão",
+            "Henrique IV",
+            "Tempestade",
+            "Macbeth"
+
+
+    };
+
+}
